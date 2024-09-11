3b705ba6f79faab183507d451686e23cb88c47de
Ludovic VAGNER <lvagn@lmdsio.com>
2024-09-11 14:25:15 +0200

    mon projet version 2

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,4 +1,32 @@
 <?php
+if (empty($_POST['login']) || (empty($_POST['password'])) ||
+($_POST['login'] != 'admin' ) || ($_POST['password']) != 'azerty')
+{
+    sleep(1);
+ header('Location : http://localhost/lvagn/GitExo1/index.html');
+ exit;
+}
+
+//if (((isset($_GET['nom'])) && (!empty($_GET['nom']))) && ((isset($_GET['prenom'])) && (!empty($_GET['prenom']))))
+//{
+//    $nom = $_GET['nom'];
+//    $prenom = $_GET['prenom'];
+//}
+//else
+//{
+//    $nom = 'nom';
+//    $prenom = 'prenom';
+//}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Accueil</title>
+    <meta lang="fr">
+</head>
+<body>
+<?php
 if (((isset($_GET['nom'])) && (!empty($_GET['nom']))) && ((isset($_GET['prenom'])) && (!empty($_GET['prenom']))))
 {
     $nom = $_GET['nom'];
@@ -10,14 +38,6 @@ else
     $prenom = 'prenom';
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8"/>
-    <title>Accueil</title>
-    <meta lang="fr">
-</head>
-<body>
 <h1> Bonjour <?=$nom.' '. $prenom?></h1>
 </body>
 </html>
